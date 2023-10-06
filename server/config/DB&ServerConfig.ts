353d4bea6a1ee1902748bdbb7d761559ad6a1b2a
Abdulrahman Fahmy <abdulrhmanabotor@gmail.com>
2023-10-06 13:00:02 +0300

    added DB&serverconfig

---
+++ server/config/DB&ServerConfig.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config()
+let BACKEND_SERVER_PORT:Number = 3000;
+const MONGO_URI: String = process.env.MONGO_URI || "";
+
+if (process.env.PORT)
+    BACKEND_SERVER_PORT = Number(process.env.PORT);
+
+
+export const config = {
+
+    server: {port:BACKEND_SERVER_PORT},
+    DB: {url:MONGO_URI}
+}
+
+
+
