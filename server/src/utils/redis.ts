a3427bfa22a6bd2680235df9e96f45fb8d84b6bd
Mark Mahrous <markmahrous012@gmail.com>
2023-10-09 16:59:00 +0300

    previous redis

--- server/src/utils/redis.ts
+++ server/src/utils/redis.ts
@@ -2,40 +2,31 @@ import Redis from 'ioredis';
 import { StatusCodes } from 'http-status-codes';
 import HttpError from './HttpError';
 import dotenv from 'dotenv';
-
 dotenv.config();
 const REDIS_PASSWORD = process.env.REDIS_SECRET_PASS;
-
 const redisClient = new Redis({
   password: REDIS_PASSWORD,
   host: 'redis-15759.c135.eu-central-1-1.ec2.cloud.redislabs.com',
   port: 15759
 });
-
 redisClient.on('error', (error) => {
   console.log(`Redis error: ${error.message}`);
   throw new HttpError(StatusCodes.INTERNAL_SERVER_ERROR, 'Redis Error');
 });
-
 const putRedis = async (key: any, data: any): Promise<string> =>
   redisClient.set(JSON.stringify(key), JSON.stringify(data), 'EX', 3600);
 
-interface Key {
-  [key: string]: any;
-}
+const getRedis = async (key: any): Promise<string | null> => redisClient.get(JSON.stringify(key));
 
-const getRedis = async (key: Key): Promise<string | null> => redisClient.get(JSON.stringify(key));
+const delRedis = async (key: any): Promise<number> => redisClient.del(JSON.stringify(key));
 
-const delRedis = async (key: Key): Promise<number> => redisClient.del(JSON.stringify(key));
+const clearRedis = async () => redisClient.flushall();
 
-const clearRedis = async (): Promise<string> => redisClient.flushall();
-
-const keyGenerator = (reqKey: Key): Key => {
+const keyGenerator = (reqKey: object): object => {
   const sortedKeys = Object.keys(reqKey).sort();
-  const sortedKey: Key = {};
+  const sortedKey: object = {};
   for (const key of sortedKeys) sortedKey[key] = reqKey[key];
 
   return sortedKey;
 };
-
 export { putRedis, getRedis, delRedis, clearRedis, keyGenerator };
