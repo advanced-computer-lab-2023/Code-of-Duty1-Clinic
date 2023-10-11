36a3fcb86b553bfbdaafcd27e1c7ad6017fab3cb
BolesWaheed <boleswaheed477@gmail.com>
2023-10-12 01:23:24 +0300

    add register from abdallad branch

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -2,6 +2,8 @@ import userModel, { IUser, IUserDocument } from '../models/user.model';
 import { HttpError } from '../utils';
 import { StatusCodes } from 'http-status-codes';
 import { generateToken } from '../utils';
+import Request from '../models/request.model';
+import User from '../models/user.model';
 const login = async (username: string, password: string) => {
   console.log(username, password);
   if (!(username && password)) throw new HttpError(StatusCodes.BAD_REQUEST, 'Username and password are required');
@@ -19,8 +21,28 @@ const login = async (username: string, password: string) => {
 const logout = () => {
   return 'Logout';
 };
-const register = (info: any) => {
-  return 'Register';
+const register = async (body: any) => {
+  const user = new User(body);
+  await user.save();
+  if (body.role === 'Doctor') {
+    const newRequest = new Request({
+      medicID: user._id,
+      ID: body.id,
+      degree: body.degree,
+      licenses: body.licenses,
+      status: 'Pending',
+      date: new Date()
+    });
+    await newRequest.save();
+  }
+  // let token = generateToken(user._id,body.role);
+  // putRedis(token,token)
+  return {
+    status: StatusCodes.CREATED,
+    message: 'User created successfully',
+    result: user
+    // token:token
+  };
 };
 const changePassword = (username: string, oldPassword: string, newPassword: string) => {
   return 'Change Password';
