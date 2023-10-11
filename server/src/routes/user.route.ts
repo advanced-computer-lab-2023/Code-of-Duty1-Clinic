d6235981075d52aee198e442d00e9f45ffa1a36d
BolesWaheed <boleswaheed477@gmail.com>
2023-10-11 23:15:58 +0300

    add update info service

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -6,9 +6,13 @@ import { isAuthenticated, isAuthorized, queryParser } from '../middlewares';
 
 // import the user service
 import { getPatients, selectPatient } from '../services/userService';
+import { updateInfo } from '../services/user/user.service';
 // http methods required for this router
 
 router.get('/me/patient/', async (req, res) => controller(res)(getPatients)(req.query.doctorID));
 router.get('/me/patient/:id', async (req, res) => controller(res)(selectPatient)(req.query.doctorID, req.params.id));
+router.put('/me/info/', (req, res) => {
+  controller(res)(updateInfo)(req.body);
+});
 
 export default router;
