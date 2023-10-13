b82ca90b7cea1460361412ccac2e9bc7899c521f
AliAdam102002 <aelwahussein@gmail.com>
2023-10-13 19:46:34 +0300

    added_some_functionality_to_frontEnd

--- server/src/routes/package.route.ts
+++ server/src/routes/package.route.ts
@@ -1,23 +1,23 @@
-import express from "express";
-import controller from "../controllers/controller";
-import { isAuthorized } from "../middlewares";
-import {
-  addPackage,
-  updatePackage,
-  deletePackage,
-} from "../services/package/package.service";
+import express from 'express';
+import controller from '../controllers/controller';
+import { isAuthorized } from '../middlewares';
+import { getPackages, addPackage, updatePackage, deletePackage } from '../services/package/package.service';
 const router = express.Router();
 // http methods required for this router
 
 // router.use(isAuthorized("Administrator"));
-router.post("/", (req, res) => {
+router.get('/', (req, res) => {
+  controller(res)(getPackages)(req.query);
+});
+
+router.post('/', (req, res) => {
   controller(res)(addPackage)(req.body);
 });
-router.put("/:id", (req, res) => {
-  console.log("tell me if i go there or nots");
+router.put('/:id', (req, res) => {
+  console.log('tell me if i go there or nots');
   controller(res)(updatePackage)(req.params.id, req.body);
 });
-router.delete("/:id", (req, res) => {
+router.delete('/:id', (req, res) => {
   controller(res)(deletePackage)(req.params.id);
 });
 
