28d7fa37e7ca3d89494ed0fe90fcbf47d10ee628
Zeyad Hesham <zeyadhesham53@gmail.com>
2023-10-24 00:30:01 +0300

    writting req 45, 46 without testing

--- server/src/services/appointment.service.ts
+++ server/src/services/appointment.service.ts
@@ -1,4 +1,4 @@
-import { Appointment } from '../models';
+import { Appointment, Patient, Doctor } from '../models';
 import { StatusCodes } from 'http-status-codes';
 import mongoose from 'mongoose';
 import { HttpError } from '../utils';
@@ -18,4 +18,150 @@ const getAppointments = async (query: any) => {
   };
 };
 
-export { getAppointments };
+// Function to get upcoming appointments for a user (patient or doctor) req 45
+const getUpcomingAppointments = async (userId: string) => {
+  try {
+    // Check if the user is a doctor
+    const isDoctor = await Doctor.exists({ _id: userId });
+
+    if (isDoctor) {
+      // If it's a doctor, retrieve the doctor's appointments
+      return {
+        status: StatusCodes.OK,
+        message: 'Appointments retrieved successfully',
+        result: await Appointment.find({ doctorID: userId, startTime: { $gte: new Date() } }),
+      };
+
+
+    } else {
+      // If not a doctor, check if it's a patient
+      const isPatient = await Patient.exists({ _id: userId });
+
+      if (isPatient) {
+        // If it's a patient, retrieve the patient's appointments
+        return {
+          status: StatusCodes.OK,
+          message: 'Appointments retrieved successfully',
+          result: await Appointment.find({ patientID: userId, startTime: { $gte: new Date() } }),
+        };
+
+
+      } else {
+        throw new HttpError(
+          StatusCodes.BAD_REQUEST,
+          'User is neither a doctor nor a patient'
+        );
+      }
+    }
+
+  } catch (error) {
+    throw new HttpError(
+      StatusCodes.BAD_REQUEST,
+      'User is neither a doctor nor a patient'
+    );
+  }
+};
+
+
+
+// Function to get past appointments for a user (patient or doctor) contiue req 45
+const getPastAppointments = async (userId: string) => {
+  try {
+    // Check if the user is a doctor
+    const isDoctor = await Doctor.exists({ _id: userId });
+
+    if (isDoctor) {
+      // If it's a doctor, retrieve the doctor's past appointments
+      return {
+        status: StatusCodes.OK,
+        message: 'Past appointments retrieved successfully',
+        result: await Appointment.find({ doctorID: userId, endTime: { $lt: new Date() } }),
+      };
+    } else {
+      // If not a doctor, check if it's a patient
+      const isPatient = await Patient.exists({ _id: userId });
+
+      if (isPatient) {
+        // If it's a patient, retrieve the patient's past appointments
+        return {
+          status: StatusCodes.OK,
+          message: 'Past appointments retrieved successfully',
+          result: await Appointment.find({ patientID: userId, endTime: { $lt: new Date() } }),
+        };
+      } else {
+        throw new HttpError(
+          StatusCodes.BAD_REQUEST,
+          'User is neither a doctor nor a patient'
+        );
+      }
+    }
+  } catch (error) {
+    throw new HttpError(
+      StatusCodes.BAD_REQUEST,
+      'User is neither a doctor nor a patient'
+    );
+  }
+};
+
+// Function to filter appointments by date or status (upcoming, completed, cancelled, rescheduled) req 46
+const filterAppointments = async (query: any) => {
+  try {
+    // Check if the user is a doctor
+    const isDoctor = await Doctor.exists({ _id: query.userId });
+    let finalresult: any[] = [];
+    if (isDoctor) {
+      // If it's a doctor, retrieve the doctor's appointments
+      let appointments: any = getAppointments;
+      if (query.status) {
+        appointments.result.filter((appointment: any) => {
+          if (appointment.status === query.status) {
+            finalresult.push(appointment);
+          }
+
+        });
+      } else {
+        finalresult = appointments.result;
+      }
+      return {
+        status: StatusCodes.OK,
+        message: 'Appointments retrieved successfully',
+        result: finalresult,
+      };
+
+    } else {
+      // If not a doctor, check if it's a patient
+      const isPatient = await Patient.exists({ _id: query.userId });
+
+      if (isPatient) {
+        let appointments: any = getAppointments;
+        if (query.status) {
+          appointments.result.filter((appointment: any) => {
+            if (appointment.status === query.status) {
+              finalresult.push(appointment);
+            }
+
+          });
+        } else {
+          finalresult = appointments.result;
+        }
+        return {
+          status: StatusCodes.OK,
+          message: 'Appointments retrieved successfully',
+          result: finalresult,
+        };
+      } else {
+        throw new HttpError(
+          StatusCodes.BAD_REQUEST,
+          'User is neither a doctor nor a patient'
+        );
+      }
+    }
+  } catch (error) {
+    throw new HttpError(
+      StatusCodes.BAD_REQUEST,
+      'User is neither a doctor nor a patient'
+    );
+  }
+};
+
+export { getAppointments, getUpcomingAppointments, getPastAppointments };
