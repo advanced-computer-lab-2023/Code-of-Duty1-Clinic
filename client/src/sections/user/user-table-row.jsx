e7c5055ddceb00d99d90a3cbb3990a01e07e0d8d
Zeyad Hesham <zeyadhesham53@gmail.com>
2023-11-11 18:39:42 +0200

    changing user page to appointment page with getpast or get upcoming

--- client/src/sections/user/user-table-row.jsx
+++ client/src/sections/user/user-table-row.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-
 import Stack from '@mui/material/Stack';
 import Avatar from '@mui/material/Avatar';
 import Popover from '@mui/material/Popover';
@@ -10,20 +9,19 @@ import MenuItem from '@mui/material/MenuItem';
 import TableCell from '@mui/material/TableCell';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-
 import Label from 'src/components/label';
 import Iconify from 'src/components/iconify';
 
-// ----------------------------------------------------------------------
-
 export default function UserTableRow({
   selected,
-  name,
-  avatarUrl,
-  company,
-  role,
-  isVerified,
+  _id,
+  doctorID,
+  patientID,
   status,
+  sessionPrice,
+  startDate,
+  endDate,
+  isFollowUp,
   handleClick,
 }) {
   const [open, setOpen] = useState(null);
@@ -45,61 +43,41 @@ export default function UserTableRow({
 
         <TableCell component="th" scope="row" padding="none">
           <Stack direction="row" alignItems="center" spacing={2}>
-            <Avatar alt={name} src={avatarUrl} />
+            <Avatar alt={doctorID} src={doctorID} />
             <Typography variant="subtitle2" noWrap>
-              {name}
+              {doctorID}
             </Typography>
           </Stack>
         </TableCell>
 
-        <TableCell>{company}</TableCell>
+        <TableCell>{patientID}</TableCell>
 
-        <TableCell>{role}</TableCell>
+        <TableCell>{status}</TableCell>
 
-        <TableCell align="center">{isVerified ? 'Yes' : 'No'}</TableCell>
+        <TableCell>{sessionPrice}</TableCell>
 
-        <TableCell>
-          <Label color={(status === 'banned' && 'error') || 'success'}>{status}</Label>
-        </TableCell>
+        <TableCell>{startDate}</TableCell>
 
-        <TableCell align="right">
-          <IconButton onClick={handleOpenMenu}>
-            <Iconify icon="eva:more-vertical-fill" />
-          </IconButton>
-        </TableCell>
+        <TableCell>{endDate}</TableCell>
+
+        <TableCell>{isFollowUp ? 'Yes' : 'No'}</TableCell>
+
+       
       </TableRow>
 
-      <Popover
-        open={!!open}
-        anchorEl={open}
-        onClose={handleCloseMenu}
-        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        PaperProps={{
-          sx: { width: 140 },
-        }}
-      >
-        <MenuItem onClick={handleCloseMenu}>
-          <Iconify icon="eva:edit-fill" sx={{ mr: 2 }} />
-          Edit
-        </MenuItem>
-
-        <MenuItem onClick={handleCloseMenu} sx={{ color: 'error.main' }}>
-          <Iconify icon="eva:trash-2-outline" sx={{ mr: 2 }} />
-          Delete
-        </MenuItem>
-      </Popover>
     </>
   );
 }
 
 UserTableRow.propTypes = {
-  avatarUrl: PropTypes.any,
-  company: PropTypes.any,
-  handleClick: PropTypes.func,
-  isVerified: PropTypes.any,
-  name: PropTypes.any,
-  role: PropTypes.any,
-  selected: PropTypes.any,
+  _id: PropTypes.string.isRequired,
+  doctorID: PropTypes.string,
+  patientID: PropTypes.string,
   status: PropTypes.string,
+  sessionPrice: PropTypes.number,
+  startDate: PropTypes.string,
+  endDate: PropTypes.string,
+  isFollowUp: PropTypes.bool,
+  selected: PropTypes.bool,
+  handleClick: PropTypes.func,
 };
--- client/src/sections/user/view/user-view.jsx
+++ client/src/sections/user/view/user-view.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
 import Table from '@mui/material/Table';
@@ -9,33 +8,35 @@ import TableBody from '@mui/material/TableBody';
 import Typography from '@mui/material/Typography';
 import TableContainer from '@mui/material/TableContainer';
 import TablePagination from '@mui/material/TablePagination';
-
-import { users } from 'src/_mock/user';
-
-import Iconify from 'src/components/iconify';
-import Scrollbar from 'src/components/scrollbar';
-
 import TableNoData from '../table-no-data';
 import UserTableRow from '../user-table-row';
 import UserTableHead from '../user-table-head';
 import TableEmptyRows from '../table-empty-rows';
-import UserTableToolbar from '../user-table-toolbar';
-import { emptyRows, applyFilter, getComparator } from '../utils';
-
-// ----------------------------------------------------------------------
+import { emptyRows, getComparator } from '../utils';
+import { axiosInstance } from '../../../utils/axiosInstance';
+import Iconify from 'src/components/iconify';
+import Scrollbar from 'src/components/scrollbar';
 
 export default function UserPage() {
+  const [appointments, setAppointments] = useState([]);
   const [page, setPage] = useState(0);
-
   const [order, setOrder] = useState('asc');
-
   const [selected, setSelected] = useState([]);
-
-  const [orderBy, setOrderBy] = useState('name');
-
-  const [filterName, setFilterName] = useState('');
-
+  const [orderBy, setOrderBy] = useState('doctorID'); // Set default ordering based on doctorID
   const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  useEffect(() => {
+    const fetchAppointments = async () => {
+      try {
+        const response = await axiosInstance.get('/me/appointments');
+        setAppointments(response.data.result);
+      } catch (error) {
+        console.error('Error fetching appointments:', error);
+      }
+    };
+
+    fetchAppointments();
+  }, []); // Fetch appointments on component mount
 
   const handleSort = (event, id) => {
     const isAsc = orderBy === id && order === 'asc';
@@ -47,18 +48,18 @@ export default function UserPage() {
 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
-      const newSelecteds = users.map((n) => n.name);
+      const newSelecteds = appointments.map((n) => n.doctorID);
       setSelected(newSelecteds);
       return;
     }
     setSelected([]);
   };
 
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
+  const handleClick = (event, doctorID) => {
+    const selectedIndex = selected.indexOf(doctorID);
     let newSelected = [];
     if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
+      newSelected = newSelected.concat(selected, doctorID);
     } else if (selectedIndex === 0) {
       newSelected = newSelected.concat(selected.slice(1));
     } else if (selectedIndex === selected.length - 1) {
@@ -81,78 +82,109 @@ export default function UserPage() {
     setRowsPerPage(parseInt(event.target.value, 10));
   };
 
-  const handleFilterByName = (event) => {
-    setPage(0);
-    setFilterName(event.target.value);
+  const handleGetUpcomingAppointments = async () => {
+    try {
+      const response = await axiosInstance.get('/me/appointments?s=Upcoming');
+      setAppointments(response.data.result);
+    } catch (error) {
+      console.error('Error fetching upcoming appointments:', error);
+    }
   };
 
-  const dataFiltered = applyFilter({
-    inputData: users,
-    comparator: getComparator(order, orderBy),
-    filterName,
-  });
-
-  const notFound = !dataFiltered.length && !!filterName;
-
+  const handleGetPastAppointments = async () => {
+    try {
+      const response = await axiosInstance.get('/me/appointments?s=Completed');
+      setAppointments(response.data.result);
+    } catch (error) {
+      console.error('Error fetching past appointments:', error);
+    }
+  };
+  
+  const handleGetAllAppointments = async () => {
+    try {
+      const response = await axiosInstance.get('/me/appointments');
+      setAppointments(response.data.result);
+    } catch (error) {
+      console.error('Error fetching all appointments:', error);
+    }
+  };
   return (
     <Container>
       <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-        <Typography variant="h4">Users</Typography>
+        <Typography variant="h4">Appointments</Typography>
 
-        <Button variant="contained" color="inherit" startIcon={<Iconify icon="eva:plus-fill" />}>
-          New User
+        <Button
+          variant="contained"
+          color="inherit"
+          sstartIcon={<Iconify icon="eva:plus-fill" />}
+          onClick={handleGetAllAppointments}
+        >
+          Get All Appointments
+        </Button>
+
+        <Button
+          variant="contained"
+          color="inherit"
+          sstartIcon={<Iconify icon="eva:plus-fill" />}
+          onClick={handleGetPastAppointments}
+        >
+          Get Past Appointments
+        </Button>
+        <Button
+          variant="contained"
+          color="inherit"
+          sstartIcon={<Iconify icon="eva:plus-fill" />}
+          onClick={handleGetUpcomingAppointments}
+        >
+          Get Upcoming Appointments
         </Button>
       </Stack>
 
       <Card>
-        <UserTableToolbar
-          numSelected={selected.length}
-          filterName={filterName}
-          onFilterName={handleFilterByName}
-        />
-
         <Scrollbar>
           <TableContainer sx={{ overflow: 'unset' }}>
             <Table sx={{ minWidth: 800 }}>
               <UserTableHead
                 order={order}
                 orderBy={orderBy}
-                rowCount={users.length}
+                rowCount={appointments.length}
                 numSelected={selected.length}
                 onRequestSort={handleSort}
                 onSelectAllClick={handleSelectAllClick}
                 headLabel={[
-                  { id: 'name', label: 'Name' },
-                  { id: 'company', label: 'Company' },
-                  { id: 'role', label: 'Role' },
-                  { id: 'isVerified', label: 'Verified', align: 'center' },
+                  { id: 'doctorID', label: 'Doctor ID' },
+                  { id: 'patientID', label: 'Patient ID' },
                   { id: 'status', label: 'Status' },
-                  { id: '' },
+                  { id: 'sessionPrice', label: 'Session Price' },
+                  { id: 'startDate', label: 'Start Date' },
+                  { id: 'endDate', label: 'End Date' },
+                  { id: 'isFollowUp', label: 'Follow Up' },
+                
                 ]}
               />
               <TableBody>
-                {dataFiltered
+                {appointments
                   .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                   .map((row) => (
                     <UserTableRow
-                      key={row.id}
-                      name={row.name}
-                      role={row.role}
+                      key={row._id}
+                      _id={row._id}
+                      doctorID={row.doctorID}
+                      patientID={row.patientID}
                       status={row.status}
-                      company={row.company}
-                      avatarUrl={row.avatarUrl}
-                      isVerified={row.isVerified}
-                      selected={selected.indexOf(row.name) !== -1}
-                      handleClick={(event) => handleClick(event, row.name)}
+                      sessionPrice={row.sessionPrice}
+                      startDate={row.startDate}
+                      endDate={row.endDate}
+                      isFollowUp={row.isFollowUp}
+                      selected={selected.indexOf(row.doctorID) !== -1}
+                      handleClick={(event) => handleClick(event, row.doctorID)}
                     />
                   ))}
 
                 <TableEmptyRows
                   height={77}
-                  emptyRows={emptyRows(page, rowsPerPage, users.length)}
+                  emptyRows={emptyRows(page, rowsPerPage, appointments.length)}
                 />
-
-                {notFound && <TableNoData query={filterName} />}
               </TableBody>
             </Table>
           </TableContainer>
@@ -161,7 +193,7 @@ export default function UserPage() {
         <TablePagination
           page={page}
           component="div"
-          count={users.length}
+          count={appointments.length}
           rowsPerPage={rowsPerPage}
           onPageChange={handleChangePage}
           rowsPerPageOptions={[5, 10, 25]}
