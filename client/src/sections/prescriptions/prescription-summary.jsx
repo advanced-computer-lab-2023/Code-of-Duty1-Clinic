2d71f45576a5c173c13ced1fe9af70fee5903bd0
Mark Mahrous <markmahrous012@gmail.com>
2023-12-11 20:34:43 +0200

    handle task 59 which is to download prescription as PDF

--- client/src/sections/prescriptions/prescription-summary.jsx
+++ client/src/sections/prescriptions/prescription-summary.jsx
@@ -9,6 +9,8 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import SaveIcon from '@mui/icons-material/Save';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import React, { useRef } from 'react';
+import html2pdf from 'html2pdf.js';
 
 import { axiosInstance } from '../../utils/axiosInstance';
 import { Select, Input, Button } from '@mui/material';
@@ -29,6 +31,24 @@ export default function PrescriptionSummary({
   fetchPrescriptions,
   ...other
 }) {
+  const printableContentRef = useRef(null);
+
+  const handleDownload = () => {
+    const content = printableContentRef.current;
+
+    if (!content) return;
+
+    const opt = {
+      margin: 0.5,
+      filename: `Prescription_${prescriptionID}.pdf`,
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    html2pdf().from(content).set(opt).save();
+  };
+
   const user = localStorage.getItem('userRole');
   console.log(user);
 
@@ -158,167 +178,193 @@ export default function PrescriptionSummary({
   };
 
   return (
-    <Card sx={{ p: 2, maxWidth: 600, margin: 'auto', boxShadow: 3, ...sx }} {...other}>
-      <Stack spacing={2}>
-        <Typography variant="h6">Prescription Summary</Typography>
-        <Box>
-          {editingDescription ? (
-            <div>
-              <input
-                type="text"
-                value={editedDescription}
-                onChange={(event) => setEditedDescription(event.target.value)}
-              />
-              <IconButton aria-label="save" color="primary" onClick={handleEditDescription}>
-                <SaveIcon />
-              </IconButton>
-            </div>
-          ) : (
-            <Typography
-              variant="body1"
-              gutterBottom
-              onDoubleClick={() => {
-                if (user === 'Doctor' && !isSubmitted) setEditingDescription(true);
-              }}
-            >
-              <strong>Description:</strong> {description}
-            </Typography>
-          )}
-          <Typography variant="subtitle1">
-            <strong>Date:</strong> {date}
-          </Typography>
-          <Typography variant="subtitle1">
-            <strong>Doctor:</strong> {doctorName}
-          </Typography>
-          <Typography variant="subtitle1">
-            <strong>Patient:</strong> {patientName}
-          </Typography>
-          <br />
-          {isFilled ? (
-            <Typography variant="subtitle1">
-              <strong>Status:</strong> Filled
-            </Typography>
-          ) : (
-            <Typography variant="subtitle1">
-              <strong>Status:</strong> Not Filled
-            </Typography>
-          )}
-          {isSubmitted ? (
-            <Typography variant="subtitle1">
-              <strong>Submission Status:</strong> Submitted
-            </Typography>
-          ) : (
-            <Typography variant="subtitle1">
-              <strong>Submission Status:</strong> Not Submitted
-            </Typography>
-          )}
-        </Box>
-        <Typography variant="body1">
-          <strong>Medicines:</strong>
-        </Typography>
-        <Stack spacing={2}>
-          {medicines.map((medicineData) => (
-            <Box
-              key={medicineData._id}
-              sx={{ border: '1px solid #ccc', p: 2, borderRadius: 4, display: 'flex', alignItems: 'center' }}
-            >
-              {editingMedicineId === medicineData._id ? (
-                <Box sx={{ flexGrow: 1 }}>
-                  <input type="text" value={editedDosage} onChange={(event) => setEditedDosage(event.target.value)} />
-                  <IconButton
-                    aria-label="save"
-                    color="primary"
-                    onClick={() => handleEditMedicineDosage(medicineData._id)}
-                  >
+    <div>
+      <div ref={printableContentRef}>
+        <Card sx={{ p: 2, maxWidth: 600, margin: 'auto', boxShadow: 3, ...sx }} {...other}>
+          <Stack spacing={2}>
+            <Typography variant="h6">Prescription Summary</Typography>
+            <Box>
+              {editingDescription ? (
+                <div>
+                  <input
+                    type="text"
+                    value={editedDescription}
+                    onChange={(event) => setEditedDescription(event.target.value)}
+                  />
+                  <IconButton aria-label="save" color="primary" onClick={handleEditDescription}>
                     <SaveIcon />
                   </IconButton>
-                </Box>
+                </div>
               ) : (
-                <>
-                  <Box sx={{ flexGrow: 1 }}>
-                    <Typography variant="body1">
-                      <strong>Medicine:</strong> {medicineData.medicine}
-                    </Typography>
-                    <Typography variant="body1">
-                      <strong>Dosage:</strong> {medicineData.dosage}
-                    </Typography>
-                    <Typography variant="body1">
-                      <strong>IsSubmitted:</strong> {medicineData.isSubmitted ? 'Yes' : 'No'}
-                    </Typography>
-                  </Box>
-                  {user === 'Doctor' && (
-                    <div>
-                      <IconButton
-                        aria-label="edit"
-                        color="primary"
-                        onClick={() => {
-                          setEditingMedicineId(medicineData._id);
-                          setEditedDosage(medicineData.dosage);
-                        }}
-                        disabled={medicineData.isSubmitted || isSubmitted}
-                      >
-                        <EditIcon />
-                      </IconButton>
-                      <IconButton
-                        aria-label="delete"
-                        color="error"
-                        onClick={() => handleDeleteMedicine(medicineData._id)}
-                        disabled={medicineData.isSubmitted || isSubmitted}
-                      >
-                        <DeleteIcon />
-                      </IconButton>
-                    </div>
-                  )}
-                  {user === 'Patient' && (
-                    <div>
-                      <IconButton
-                        aria-label="add"
-                        color="primary"
-                        onClick={() => handleAddCartItem(medicineData.medicine, medicineData._id)}
-                        // check if date is a week before
-                        disabled={medicineData.isSubmitted || checkDate()}
-                      >
-                        <AddCircleOutlineIcon />
-                      </IconButton>
-                    </div>
-                  )}
-                </>
+                <Typography
+                  variant="body1"
+                  gutterBottom
+                  onDoubleClick={() => {
+                    if (user === 'Doctor' && !isSubmitted) setEditingDescription(true);
+                  }}
+                >
+                  <strong>Description:</strong> {description}
+                </Typography>
+              )}
+              <Typography variant="subtitle1">
+                <strong>Date:</strong> {date}
+              </Typography>
+              <Typography variant="subtitle1">
+                <strong>Doctor:</strong> {doctorName}
+              </Typography>
+              <Typography variant="subtitle1">
+                <strong>Patient:</strong> {patientName}
+              </Typography>
+              <br />
+              {isFilled ? (
+                <Typography variant="subtitle1">
+                  <strong>Status:</strong> Filled
+                </Typography>
+              ) : (
+                <Typography variant="subtitle1">
+                  <strong>Status:</strong> Not Filled
+                </Typography>
+              )}
+              {isSubmitted ? (
+                <Typography variant="subtitle1">
+                  <strong>Submission Status:</strong> Submitted
+                </Typography>
+              ) : (
+                <Typography variant="subtitle1">
+                  <strong>Submission Status:</strong> Not Submitted
+                </Typography>
               )}
             </Box>
-          ))}
-        </Stack>
-        {user === 'Doctor' && !isSubmitted && (
-          <div>
-            <Typography variant="h6">Add Medicine</Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
-              <Typography variant="body1">
-                <label htmlFor="medicineInput">Medicine: </label>
-                <select id="medicineInput" onChange={(event) => setNewMedicine(event.target.value)}>
-                  <option value="">Select Medicine</option>
-                  {medicinesListNames.map((medicineName, index) => (
-                    <option key={index} value={medicineName}>
-                      {medicineName}
-                    </option>
-                  ))}
-                </select>
-              </Typography>
-              <Typography variant="body1">
-                <label htmlFor="dosageInput">Dosage: </label>
-                <Input
-                  id="dosageInput"
-                  onChange={(event) => {
-                    setNewDosage(event.target.value);
-                  }}
-                />
-              </Typography>
-              <Button variant="contained" onClick={handleAddMedicine}>
-                Add Medicine
-              </Button>
-            </Box>
-          </div>
-        )}
-      </Stack>
-    </Card>
+            <Typography variant="body1">
+              <strong>Medicines:</strong>
+            </Typography>
+            <Stack spacing={2}>
+              {medicines.map((medicineData) => (
+                <Box
+                  key={medicineData._id}
+                  sx={{ border: '1px solid #ccc', p: 2, borderRadius: 4, display: 'flex', alignItems: 'center' }}
+                >
+                  {editingMedicineId === medicineData._id ? (
+                    <Box sx={{ flexGrow: 1 }}>
+                      <input
+                        type="text"
+                        value={editedDosage}
+                        onChange={(event) => setEditedDosage(event.target.value)}
+                      />
+                      <IconButton
+                        aria-label="save"
+                        color="primary"
+                        onClick={() => handleEditMedicineDosage(medicineData._id)}
+                      >
+                        <SaveIcon />
+                      </IconButton>
+                    </Box>
+                  ) : (
+                    <>
+                      <Box sx={{ flexGrow: 1 }}>
+                        <Typography variant="body1">
+                          <strong>Medicine:</strong> {medicineData.medicine}
+                        </Typography>
+                        <Typography variant="body1">
+                          <strong>Dosage:</strong> {medicineData.dosage}
+                        </Typography>
+                        <Typography variant="body1">
+                          <strong>IsSubmitted:</strong> {medicineData.isSubmitted ? 'Yes' : 'No'}
+                        </Typography>
+                      </Box>
+                      {user === 'Doctor' && (
+                        <div>
+                          <IconButton
+                            aria-label="edit"
+                            color="primary"
+                            onClick={() => {
+                              setEditingMedicineId(medicineData._id);
+                              setEditedDosage(medicineData.dosage);
+                            }}
+                            disabled={medicineData.isSubmitted || isSubmitted}
+                          >
+                            <EditIcon />
+                          </IconButton>
+                          <IconButton
+                            aria-label="delete"
+                            color="error"
+                            onClick={() => handleDeleteMedicine(medicineData._id)}
+                            disabled={medicineData.isSubmitted || isSubmitted}
+                          >
+                            <DeleteIcon />
+                          </IconButton>
+                        </div>
+                      )}
+                      {user === 'Patient' && (
+                        <div>
+                          <IconButton
+                            aria-label="add"
+                            color="primary"
+                            onClick={() => handleAddCartItem(medicineData.medicine, medicineData._id)}
+                            // check if date is a week before
+                            disabled={medicineData.isSubmitted || checkDate()}
+                          >
+                            <AddCircleOutlineIcon />
+                          </IconButton>
+                        </div>
+                      )}
+                    </>
+                  )}
+                </Box>
+              ))}
+            </Stack>
+            {user === 'Doctor' && !isSubmitted && (
+              <div>
+                <Typography variant="h6">Add Medicine</Typography>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+                  <Typography variant="body1">
+                    <label htmlFor="medicineInput">Medicine: </label>
+                    <select id="medicineInput" onChange={(event) => setNewMedicine(event.target.value)}>
+                      <option value="">Select Medicine</option>
+                      {medicinesListNames.map((medicineName, index) => (
+                        <option key={index} value={medicineName}>
+                          {medicineName}
+                        </option>
+                      ))}
+                    </select>
+                  </Typography>
+                  <Typography variant="body1">
+                    <label htmlFor="dosageInput">Dosage: </label>
+                    <Input
+                      id="dosageInput"
+                      onChange={(event) => {
+                        setNewDosage(event.target.value);
+                      }}
+                    />
+                  </Typography>
+                  <Button variant="contained" onClick={handleAddMedicine}>
+                    Add Medicine
+                  </Button>
+                </Box>
+              </div>
+            )}
+          </Stack>
+        </Card>
+      </div>
+      <button
+        style={{
+          padding: '10px 20px',
+          borderRadius: '4px',
+          border: 'none',
+          backgroundColor: '#007bff',
+          color: '#fff',
+          cursor: 'pointer',
+          fontSize: '16px',
+          fontWeight: 'bold',
+          margin: '10px 0',
+          boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.2)',
+          transition: 'background-color 0.3s ease'
+        }}
+        onClick={handleDownload}
+      >
+        Download as PDF
+      </button>
+    </div>
   );
 }
 
